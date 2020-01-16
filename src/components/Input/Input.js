0a3ba422eb036ca205831b9164ca9028a2c14bb0
ashtinlatting <rlatting92@gwu.edu>
2020-01-15 21:31:30 -0500

    hardcoded list, set to begin working on functionality

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,12 +1,44 @@
 import React from 'react';
 
-function Input() {
-  return (
-    <div className="Input">
-      <input type="text" />
-      <button type ="submit" />
-    </div>
-  );
+class Input extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: ''
+        }
+    }
+
+    onChangeValue = e => {
+        this.setState({
+            value: e.target.value
+        });
+    }
+
+    onAddItem = () => {
+        //should i keep this state here, or move it to the same spot as list?
+    }
+
+    render() {
+        return (
+            <div className="Input">
+            <form>
+                <input 
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.onChangeValue}
+                />
+                <button 
+                    type ="submit" 
+                    onClick={this.onAddItem}
+                    disabled={this.state.value}
+                >
+                    Add
+                </button>
+            </form>
+            </div>
+        );
+    }
 }
 
 export default Input;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-function List() {
-  return (
-    <div className="List">
-      <h1>List</h1>
-    </div>
-  );
+class List extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            list : [
+                "Run credit for John Doe",
+                "Organize files for Jane Smith",
+                "Buy a Monster"
+            ]
+        };
+    }
+
+    render() {
+        const listItems = this.state.list.map((item) =>
+          <li key={item}>{item}</li>  
+        );
+
+        return (
+        <ul>{listItems}</ul>
+        );
+    }
 }
 
 export default List;
